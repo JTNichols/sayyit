466e255eb4782e76efe46bfd4e6eb091efcd6851
Jason <nichjt@gmail.com>
2026-07-07 14:42:11 -0400

    webapp workflow 2

--- sayyit.web/Program.cs
+++ sayyit.web/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace sayyit.web
+namespace Sayyit.Web
 {
     public class Program
     {
